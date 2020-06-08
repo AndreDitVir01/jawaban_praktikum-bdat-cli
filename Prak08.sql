44cc2bb3169457c6770bfd6433fced6b67231de6
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-06-08 07:06:16 +0700

    FIN:Prak08.sq;

---
+++ Prak08.sql
@@ -0,0 +1,44 @@
+--1)
+--CREATE OR REPLACE PROCEDURE add_job
+--(id jobs.job_id%TYPE, job_name jobs.job_title%TYPE) IS
+--BEGIN
+--INSERT INTO jobs(job_id, job_title)
+--VALUES(id, job_name);
+--END add_job;
+--/
+
+--BEGIN
+--add_job('IT_2D3B', 'Deny Herianto');
+--END;
+--/
+
+--SELECT * FROM jobs WHERE job_id='IT_2D3B';
+
+--2)
+--SET SERVEROUTPUT ON
+--CREATE OR REPLACE PROCEDURE upd_job(kode jobs.job_id%TYPE, job_name jobs.job_title%TYPE) IS
+--null_ex EXCEPTION;
+--BEGIN
+--UPDATE jobs SET job_title=job_name WHERE job_id=kode;
+--IF SQL%NOTFOUND THEN
+--RAISE null_ex;
+--END IF;
+--EXCEPTION
+--WHEN null_ex THEN
+--DBMS_OUTPUT.PUT_LINE('Kode jobs tidak ada');
+--END upd_job;
+--/
+
+--SELECT * FROM jobs WHERE job_id='IT_PROG';
+
+--BEGIN
+--upd_job('IT_PROG', 'DBA Administrator');
+--END;
+--/
+
+--SELECT * FROM jobs WHERE job_id='IT_PROG';
+
+--BEGIN
+--upd_job('IT_DOC', 'Doc Administrator');
+--END;
+--/
